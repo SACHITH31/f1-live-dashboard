95dabc7c888d3f2e88634603f54da6984084fc05
SACHITH <kasasachith@gmail.com>
2026-04-10 19:57:37 +0530

    added the folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -3,38 +3,69 @@ import { useEffect, useRef, useState } from "react"
 function App() {
   const canvasRef = useRef(null)
   const [cars, setCars] = useState([])
+  const prevCarsRef = useRef([])
 
   useEffect(() => {
-  const interval = setInterval(() => {
-    fetch("http://localhost:5000/api/cars")
-      .then(res => res.json())
-      .then(data => setCars(data))
-  }, 3000)
+    const interval = setInterval(() => {
+      fetch("http://localhost:5000/api/cars")
+        .then(res => res.json())
+        .then(data => {
+          prevCarsRef.current = cars
+          setCars(data)
+        })
+    }, 3000)
 
-  return () => clearInterval(interval)
-}, [])
+    return () => clearInterval(interval)
+  }, [cars])
 
   useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext("2d")
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    let progress = 0
 
-    cars.forEach(car => {
-      const x = (car.x || Math.random()) * 500
-      const y = (car.y || Math.random()) * 400
+    function animate() {
+      ctx.fillStyle = "black"
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-      ctx.beginPath()
-      ctx.arc(x, y, 5, 0, 2 * Math.PI)
-      ctx.fillStyle = "red"
-      ctx.fill()
-    })
+      cars.forEach((car, index) => {
+        const prev = prevCarsRef.current[index] || {}
+
+        const x1 = (prev.x || Math.random()) * 500
+        const y1 = (prev.y || Math.random()) * 400
+
+        const x2 = (car.x || Math.random()) * 500
+        const y2 = (car.y || Math.random()) * 400
+
+        const x = x1 + (x2 - x1) * progress
+        const y = y1 + (y2 - y1) * progress
+
+        ctx.beginPath()
+        ctx.arc(x, y, 6, 0, 2 * Math.PI)
+        ctx.fillStyle = "red"
+        ctx.fill()
+      })
+
+      progress += 0.02
+      if (progress < 1) {
+        requestAnimationFrame(animate)
+      }
+    }
+
+    animate()
   }, [cars])
 
   return (
-    <div>
-      <h1>F1 Live Dashboard 🚗</h1>
-      <canvas ref={canvasRef} width={600} height={400} style={{ border: "1px solid black" }} />
+    <div style={{ background: "black", color: "white", height: "100vh" }}>
+      <h1 style={{ textAlign: "center", color: "red" }}>
+        F1 Live Dashboard 🏎️
+      </h1>
+      <canvas
+        ref={canvasRef}
+        width={800}
+        height={500}
+        style={{ display: "block", margin: "auto", border: "1px solid red" }}
+      />
     </div>
   )
 }
